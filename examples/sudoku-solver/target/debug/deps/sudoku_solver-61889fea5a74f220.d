0162ffe94cdc96f18a551ff66965c85ff317b693
Thomas <thomas@Brazeneck-College-Bad-Blintz-Mini-2-VM.local>
2026-04-01 22:44:44 +0000

    chore: gitignore target/ in sudoku example

--- examples/sudoku-solver/target/debug/deps/sudoku_solver-61889fea5a74f220.d
+++
@@ -1,5 +0,0 @@
-/Users/thomas/Projects/lightless-labs/public/foundry/examples/sudoku-solver/target/debug/deps/sudoku_solver-61889fea5a74f220.d: green/src/main.rs
-
-/Users/thomas/Projects/lightless-labs/public/foundry/examples/sudoku-solver/target/debug/deps/sudoku_solver-61889fea5a74f220: green/src/main.rs
-
-green/src/main.rs:
