493c1daf323c5a3a0a6bd4fba2587e6e1fa77daf
El-Fitz <8971906+El-Fitz@users.noreply.github.com>
2026-04-01 22:44:44 +0000

    chore: gitignore target/ in sudoku example

--- examples/sudoku-solver/target/debug/deps/sudoku_solver-f827a050eccf6ae7.d
+++
@@ -1,5 +0,0 @@
-/Users/thomas/Projects/lightless-labs/public/foundry/examples/sudoku-solver/target/debug/deps/sudoku_solver-f827a050eccf6ae7.d: green/src/main.rs
-
-/Users/thomas/Projects/lightless-labs/public/foundry/examples/sudoku-solver/target/debug/deps/sudoku_solver-f827a050eccf6ae7: green/src/main.rs
-
-green/src/main.rs:
--- examples/sudoku-solver/target/debug/sudoku-solver.d
+++
@@ -1,1 +0,0 @@
-/Users/thomas/Projects/lightless-labs/public/foundry/examples/sudoku-solver/target/debug/sudoku-solver: /Users/thomas/Projects/lightless-labs/public/foundry/examples/sudoku-solver/green/src/main.rs
