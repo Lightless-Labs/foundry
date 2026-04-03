012f0efeedfd2de3cf315429c8d87eb52ff27dec
El-Fitz <8971906+El-Fitz@users.noreply.github.com>
2026-04-03 17:48:43 +0000

    chore: remove rubiks target/ from tracking

--- examples/rubiks-solver/target/release/rubiks-solver.d
+++
@@ -1,1 +0,0 @@
-/Users/thomas/Projects/lightless-labs/public/foundry/examples/rubiks-solver/target/release/rubiks-solver: /Users/thomas/Projects/lightless-labs/public/foundry/examples/rubiks-solver/green/src/main.rs
