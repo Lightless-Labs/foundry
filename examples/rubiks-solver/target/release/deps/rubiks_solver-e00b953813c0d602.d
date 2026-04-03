357815df7f9d87a6a4e570d4077e9a987c7fc9cc
Thomas <thomas@Brazeneck-College-Bad-Blintz-Mini-2-VM.local>
2026-04-03 17:48:43 +0000

    chore: remove rubiks target/ from tracking

--- examples/rubiks-solver/target/release/deps/rubiks_solver-e00b953813c0d602.d
+++
@@ -1,5 +0,0 @@
-/Users/thomas/Projects/lightless-labs/public/foundry/examples/rubiks-solver/target/release/deps/rubiks_solver-e00b953813c0d602.d: green/src/main.rs
-
-/Users/thomas/Projects/lightless-labs/public/foundry/examples/rubiks-solver/target/release/deps/rubiks_solver-e00b953813c0d602: green/src/main.rs
-
-green/src/main.rs:
