2b96c04db84fabd379a3c3a02f578ea1161f35d2
Thomas <thomas@Brazeneck-College-Bad-Blintz-Mini-2-VM.local>
2026-04-04 07:48:58 +0000

    chore: gitignore all target/ dirs, remove chess target

--- examples/chess-engine/target/debug/deps/chess_engine-7299274035e71c22.d
+++
@@ -1,5 +0,0 @@
-/Users/thomas/Projects/lightless-labs/public/foundry/examples/chess-engine/target/debug/deps/chess_engine-7299274035e71c22.d: green/src/main.rs
-
-/Users/thomas/Projects/lightless-labs/public/foundry/examples/chess-engine/target/debug/deps/chess_engine-7299274035e71c22: green/src/main.rs
-
-green/src/main.rs:
